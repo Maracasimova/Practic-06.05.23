f1c28375f5ec1925a11cf998e0ed6c1c21a394e0
Casimova Marina <maracasimova@gmail.com>
2023-05-08 20:30:32 +0300

    add components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,32 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { data } from '../data/users';
+import { UsersList } from './UsersList';
+import { AddUserForm } from './AddUserForm.jsx';
+import { nanoid } from 'nanoid';
+
+export class App extends Component {
+  state = { users: data };
+
+  deleteUser = id => {
+    this.setState(prevstate => {
+      return { users: prevstate.users.filter(user => user.id !== id) };
+    });
+  };
+
+  addContact = userData => {
+    const newUser = { ...userData, id: nanoid() };
+    this.setState(prevstate => {
+      return { users: [...prevstate.users, newUser] };
+    });
+  };
+
+  render() {
+    const { users } = this.state;
+    return (
+      <>
+        <UsersList users={users} deleteUser={this.deleteUser} />
+        <AddUserForm addContact={this.addContact} />
+      </>
+    );
+  }
+}
